22dee1b8cb85ae0794eec955a73edefea1066e5e
matias <patiniomatias80971a@gmail.com>
2024-10-02 15:20:49 -0300

    Agrego cantidad de productos

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,22 +1,26 @@
 
 let productos;
+let cantProductos = 0;
 
 const agregarProductos = ()=>{
     let carrito = [];
     alert('Selecione el producto que desea comprar');
 
     do{
-        productos = prompt('1 (Camiseta) - 2 (Short) - 3 (Medias) - 0 (Finalizar Compra)' );
+        productos = parseInt(prompt('1 (Camiseta) - 2 (Short) - 3 (Medias) - 0 (Finalizar Compra)'));
 
-    if(productos === '1'){
+    if(productos ===  1){
+        cantProductos++
         carrito.push('Camiseta');
         
-    }else if(productos === '2'){
+    }else if(productos === 2){
+        cantProductos++
         carrito.push('Short')
-    }else if(productos === '3'){
+    }else if(productos === 3){
+        cantProductos++
         carrito.push('Medias');
         
-    }else if(productos === '0'){
+    }else if(productos === 0){
         break;
         
     }else{
@@ -24,13 +28,13 @@ const agregarProductos = ()=>{
             alert('Por favor ingrese un producto valido');
         }
 
-    }while(productos != '0');
+    }while(productos != 0);
 
         if(carrito.length === 0){
             alert('El carrito esta vacio');
         }else{
 
-            alert('Los productos son: ' + carrito.join('-'));
+            alert('Son: ' + cantProductos + ' productos ' + ' ' + '=>' + carrito.join(' ') );
         }
 
 }
