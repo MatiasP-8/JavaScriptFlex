c6f10d0580c306ac337c0d6dca2194744c115740
matias <patiniomatias80971a@gmail.com>
2024-11-07 14:45:45 -0300

    Arreglo errores

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,52 +1,8 @@
 
-const productos = [
-    {   idProducto: 'Producto 1',
-        imagen:"./img/messiRetro.png",
-        titulo: 'Remera Lionel Messi retro',
-        descripcion:'Descripcion',
-        precio: 30000
-    },
-
-    {
-        idProducto: 'Producto 2',
-        imagen:"./img/messi2.png",
-        titulo: 'Remera Lionel Messi retro',
-        descripcion:'Descripcion',
-        precio: 40000
-    },
-    {
-        idProducto: 'Producto 3',
-        imagen:"./img/cristiano.png",
-        titulo: 'Remera Ronaldo retro',
-        descripcion:'Descripcion',
-        precio: 10000
-    }, {
-        idProducto: 'Producto 4',
-        imagen:"./img/inter.png",
-        titulo: 'Remera Inter retro',
-        descripcion:'Descripcion',
-        precio: 20000
-
-    }, {
-        idProducto: 'Producto 5',
-        imagen:"./img/arsenal.png",
-        titulo: 'Remera Arsenal retro',
-        descripcion:'Descripcion',
-        precio: 20000
-    },
-    {
-        idProducto: 'Producto 6',
-        imagen:"./img/kaka.png",
-        titulo: 'Remera Kaka retro',
-        descripcion:'Descripcion',
-        precio: 30000
-    },
-
-]
-
-
 const contenedorProductos = document.querySelector('#contenedorProductos'); //Div contenedor de las cards
 
+
+//Traigo los productos del json utilizando promesas
 
 fetch("./productos.json")
 .then(response => response.json())
@@ -72,46 +28,7 @@ fetch("./productos.json")
 })
 
 
-
-//Creo una funcion para cargar los productos en base al array de objetos que hice arriba
-// function crearProductos(){
-//     productos.forEach(producto => {
-//         const div = document.createElement('div');
-//         div.classList.add('cards'); //Le agrego la clase card al div para usar los estilos de css
-//         div.innerHTML  = `
-        
-//                         <img src="${producto.imagen}" alt="Imagen de Ropa"> 
-//                         <div class="cardInfoContainer">
-//                         <h3>${producto.titulo}</h3>
-//                         <p class="precio">$${producto.precio}</p>
-//                         <p class="descripcion">${producto.descripcion}</p>
-//                         <button class="buttonAgregar" id="${producto.idProducto}" >Agregar al carrito</button>
-//                     </div>
-
-//         `;
-
-//         contenedorProductos.appendChild(div); //Agrego el div al contenedor en el padre
-
-//     });
-
-// }
-// crearProductos(); 
-
-
-// funcion hover para desplegable de carrito
-// const carritoIcon = document.getElementById('carritoIcon');
-
-
-
-// carritoIcon.addEventListener('mouseover', mostrarMenu);
-
-// function mostrarMenu(){
-//     if(!carritoMenu.classList.contains('active')){
-//         carritoMenu.classList.toggle('active');
-//     }else if(carritoMenu.classList.contains('active')){
-//         carritoMenu.classList.toggle('active');
-//     }
-// }    
+ 
 
 
 //Verifico en productos agregados si hay productos y los traigo, para que no se me pise al traer los otros y si no hay nada, dejo el array vacio
